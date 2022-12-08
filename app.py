2adc60152c4ce90bd7cf6ad573db46a07c526d29
lonestar115 <themarx513@gmail.com>
2022-12-08 02:44:57 -0600

    Deactivated some code in app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ def home():
 	return render_template('index.html')
 
 # Start our application
-if __name__ == "__main__":
-	app.run(
-		debug=True,
+#if __name__ == "__main__":
+	#app.run(
+		#debug=True,
 		# port = 5000
-	)
+	
